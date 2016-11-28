e4a41b5dfbefee30872b1060a0aeb670d150778a
User <rahman.matikur@gmail.com>
2016-11-27 21:36:46 -0600

    commit 1

--- HelloMain.java
+++ HelloMain.java
@@ -7,7 +7,6 @@ public class HelloMain {
     public static void main(String args[]){
         System.out.println("c1");
         System.out.println("c2");
-        System.out.println("c3");
         System.out.println("c4");
 
     }
